848f6c823a94da98bce760125d19efd7cce9e529
Johan <Johangro2004@gmail.com>
2026-09-04 18:37:58 -0700

    project 1 done

--- FractalFern/main.cpp
+++ FractalFern/main.cpp
@@ -22,8 +22,15 @@
 #endif
 
 #include <stdlib.h>
-/* GLUT callback Handlers */
 
+/* global vars */
+int n = 200000;
+int num = 0;
+float x, y, oldx, oldy, a, b, c, d, e, f;
+
+
+
+/* GLUT callback Handlers */
 void resize(int width, int height)
 {
     if(width<=height)
@@ -35,6 +42,10 @@ void resize(int width, int height)
 
 void init()
 {
+ glClearColor(0,0,0,1);
+ glColor3d(0,1,0);
+ oldx = 0.5;
+ oldy = 0.5;
  glMatrixMode(GL_PROJECTION);
  glLoadIdentity();
  glOrtho(-2.5,2.75,10.5,0, -1.0, 1.0); // adjusted for suitable viewport
@@ -43,9 +54,55 @@ void init()
 
 void display(void)
 {
-  glClear (GL_COLOR_BUFFER_BIT);        // clear display screen
-  // your code here
-  glFlush ();                           // clear buffer
+    glClear (GL_COLOR_BUFFER_BIT);        // clear display screen
+
+    for (int i = 0; i < n; i++){
+        num = rand()% 100;
+        if (num < 85){      //function 2 85%: 0-84
+            a = 0.85;
+            b = -0.04;
+            c = 0.04;
+            d = 0.85;
+            e = 0;
+            f = 1.6;
+        }
+        else if (num < 92){ //function 3 7%: 85-91
+            a = 0.2;
+            b = 0.23;
+            c = -0.26;
+            d = 0.22;
+            e = 0;
+            f = 1.6;
+        }
+        else if (num < 99){ //function 4 7%: 92-98
+            a = -0.15;
+            b = 0.26;
+            c = 0.28;
+            d = 0.24;
+            e = 0;
+            f = 0.44;
+        }
+        else{               //function 1 1%: 99
+            a = 0.85;
+            b = -0.04;
+            c = 0.04;
+            d = 0.85;
+            e = 0;
+            f = 1.6;
+        }
+
+        x = a*oldx + c*oldy + e;
+        y = b*oldx + d*oldy + f;
+
+        oldx = x;
+        oldy = y;
+
+        glBegin(GL_POINTS);
+        glVertex2d(x, y);
+        glEnd();
+
+      }
+      glFlush ();                           // clear buffer
 }
 
 
